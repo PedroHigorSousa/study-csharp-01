87681ba7f0e487f23226226a6028214d9edc59a0
PedroHigorSousa <higorwork.sousa@hotmail.com>
2021-02-06 03:04:22 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace firstAplicationCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTimes = 10;
+
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                Console.WriteLine($"Interation in : {i}");
+            }
+        }
+    }
+}
